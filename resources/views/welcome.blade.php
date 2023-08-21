9788312a23c73e8410f8eba448c1ed0e272042c0
mohammedalborih <108554833+mohammedalborih@users.noreply.github.com>
2023-08-21 21:08:38 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Welcome to Back4app Contianers</title>
+        <title>البريهي يحييكم من مشروعه على السحابة</title>
 
         <!-- CSS -->
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
